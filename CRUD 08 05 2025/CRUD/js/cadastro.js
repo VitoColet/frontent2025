a7d34c0ae52e9dea4abfbbd3a8523f795bf88c17
VitoColet <146148660+VitoColet@users.noreply.github.com>
2025-05-15 22:23:56 -0300

    atualização 15 05 2025

--- CRUD 08 05 2025/CRUD/js/cadastro.js
+++ CRUD 08 05 2025/CRUD/js/cadastro.js
@@ -1,5 +1,3 @@
-
-
 const botao = document.getElementById("btnCadastrar");
 const listaUsuario = [];
 
@@ -12,4 +10,25 @@ botao.addEventListener("click", function() {
     listaUsuario.push(usuario);
     listaJson = JSON.stringify(listaUsuario);
     localStorage.setItem("usuarios", listaJson);
+    listar();
 })
+
+function listar()
+{
+    const usuariosCadastrados = JSON.parse(localStorage.getItem("usuarios")) || [];
+    let tabela = document.getElementById("lista_usuarios");
+    tabela.innerHTML = '';
+    usuariosCadastrados.forEach((usuario, index) => {
+        let linha = document.createElement("tr");
+        linha.innerHTML = `
+            <td>${usuario.usuario}</td>
+            <td>${usuario.senha}</td>
+            <td>
+            <button onclick="editarUsuario(${index})">Editar</button>
+            <button onclick="excluirUsuario(${index})">Excluir</button>
+            </td>
+        `;
+        tabela.appendChild(linha);
+    });
+}
+
